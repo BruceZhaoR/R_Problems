681609e5d68647fd5219cdfa688ca0b4b39483e7
Bruce's Thinkpad <brucezhaor2016@gmail.com>
2018-12-09 16:09:41 +0800

    update packages-update-problem.R

--- Install-Problems/packages-update-problem.R
+++ Install-Problems/packages-update-problem.R
@@ -1,3 +1,4 @@
+
 # Windows 安装问题
 
 # 找出哪些包下载了，但是没有安装成功
@@ -35,4 +36,3 @@ install.packages("path /to/*.zip", repos = NULL, type = "win.binary")
 # 需要编译的tar.gz包
 # 需要安装配置好Rtools https://mirrors.tuna.tsinghua.edu.cn/CRAN/bin/windows/Rtools/
 install.packages("path /to/tar.gz", repos = NULL, type = "source")
-
